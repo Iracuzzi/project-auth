b23ae85d611c69b6b464a116c51af2a91583b5d6
Iracuzzi <arnau.monforte@gmail.com>
2022-12-07 17:05:07 +0100

    Backend fixed

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,12 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -34,7 +35,7 @@ const User = mongoose.model("User", UserSchema);
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-
+  
   try {
     const salt = bcrypt.genSaltSync();
     if (password.length < 8) {
@@ -60,7 +61,6 @@ app.post("/register", async (req, res) => {
       });
   }
 });
-
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
@@ -130,9 +130,6 @@ app.get("/thoughts", authenticateUser);
 app.get("/thoughts", (req, res)=> {
   res.status(200).json({success: true, response: "all the thoughts"});
 });
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
